a1564279017552c28f877aed8cc7dfc06f2e088d
mirzayogy <mirza.yogy@gmail.com>
2022-06-11 16:47:17 +0800

    Membuat KabupatenViewFrame

---
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -0,0 +1,32 @@
+package frame;
+
+import javax.swing.*;
+
+public class KabupatenViewFrame  extends  JFrame{
+    private JPanel mainPanel;
+    private JTextField cariTextField;
+    private JButton cariButton;
+    private JTable viewTable;
+    private JButton tambahButton;
+    private JButton ubahButton;
+    private JButton hapusButton;
+    private JButton batalButton;
+    private JButton cetakButton;
+    private JButton tutupButton;
+    private JPanel cariPanel;
+    private JPanel buttonPanel;
+    private JScrollPane viewScrollPane;
+
+    public KabupatenViewFrame(){
+        init();
+    }
+    public void init(){
+        setContentPane(mainPanel);
+        setTitle("Data Kabupaten");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
